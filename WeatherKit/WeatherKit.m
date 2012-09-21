555a172863906291682af5c00b08328323f31309
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2012-09-21 12:49:29 -0400

    Initial Commit

---
+++ WeatherKit/WeatherKit.m
@@ -0,0 +1,13 @@
+//
+//  WeatherKit.m
+//  WeatherKit
+//
+//  Created by Ryan Nystrom on 9/21/12.
+//  Copyright (c) 2012 Ryan Nystrom. All rights reserved.
+//
+
+#import "WeatherKit.h"
+
+@implementation WeatherKit
+
+@end
